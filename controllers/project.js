d6b6b466f15c88c341445ce77cd6aeaaded01635
Rauibert <rauibertnf@gmail.com>
2020-07-30 23:58:47 +0200

    Método para guardar en la BD

--- controllers/project.js
+++ controllers/project.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var Project = require('../models/project');
+
 var controller = {
     home: function(req, res){
         return res.status(200).send({
@@ -10,6 +12,32 @@ var controller = {
     test: function(req, res){
         return res.status(200).send({
             message: 'Página Test'
+        });
+    },
+
+    saveProject: function(req, res){
+        var project = new Project();
+
+        var params = req.body;
+        project.name = params.name;
+        project.description = params.description;
+        project.category = params.category;
+        project.year = params.year;
+        project.langs = params.langs;
+        project.image = null;
+
+        project.save((err, projectStored)=>{
+            if(err) return res.status(500).send({message: 'Error al guardar'});
+
+            if(!projectStored) return res.status(404).send({message:'No se ha podido guardar'});
+
+            return res.status(200).send({project: projectStored});
+        });
+
+
+        return res.status(200).send({
+            project: project,
+            message: 'Funciona'
         });
     }
 
--- models/project.js
+++ models/project.js
@@ -8,7 +8,8 @@ var ProjectSchema = Schema({
     description: String,
     category: String,
     year: Number,
-    langs: [String]
+    langs: String,
+    image: String
 });
 
-module.exports = mongoose('Project', ProjectSchema);
+module.exports = mongoose.model('Project', ProjectSchema);
