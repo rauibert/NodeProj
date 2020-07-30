6c5672c9a530dc9d0cd353a81991909bf63dd29d
Rauibert <rauibertnf@gmail.com>
2020-07-30 14:43:18 +0200

    Modificación de modelo

---
+++ models/project.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProjectSchema = Schema({
+    name: String,
+    description: String,
+    category: String,
+    year: Number,
+    langs: [String]
+});
+
+module.exports = mongoose('Project', ProjectSchema);
