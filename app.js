ec459cdb291e0adb4f748fa3b905816a71f7ce25
Rauibert <rauibertnf@gmail.com>
2020-08-08 13:15:25 +0200

    Configuración de cabeceras y CORS

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ var projects_routes = require('./routes/project');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-// CORS
+// Configurar cabeceras y CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 // Rutas
 app.use('/api', projects_routes);
